1f0eea0723fa8ff6c67cadfe17e1a01098bc1055
Tayo Akosile <oluwatayoakosile@gmail.com>
2022-01-13 10:22:46 +0100

    Update:reated side bar options

--- components/Body.tsx
+++ components/Body.tsx
@@ -4,10 +4,11 @@ import React from "react";
 const Body = () => {
   return (
     <Box
-      border="1px solid red"
       h="100vh"
       flex="0.8"
       bg="linear-gradient(rgb(91,87,115),rgb(0,0,0,1))"
+      color='white'
+      p='30px'
     >
       BOdy
     </Box>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,29 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, chakra } from "@chakra-ui/react";
+import Image from "next/image";
 
+const ChakraImage = chakra(Image);
 const NavBar = () => {
-  return <Box as="header" border='1px' h='100vh' flex='0.2'>hello</Box>;
+  return (
+    <Box
+      as="header"
+      color="white"
+      minWidth="200px"
+      h="100vh"
+      px="10px"
+      bg="#020202"
+      flex="0.2"
+    >
+      <Box position="relative" w="120px" px="10px" h="120px">
+        <ChakraImage
+          src="/img/logo.png"
+          width="100%"
+          objectFit="contain"
+          layout="fill"
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default NavBar;
